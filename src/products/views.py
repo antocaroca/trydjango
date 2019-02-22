09bafceb494d053a17a6f7ffaee845787b1e12e1
antocaroca <antonella.caroca@cci-entel>
2019-02-22 13:03:04 -0300

    nuevo commit

--- src/products/views.py
+++ src/products/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 from .models import Product
 # Create your views here.
 
 def product_create_view(request):
-       context = {}
-       return render(request, "products/product_create.html", context)
+    my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST) # this creates an instance of that form
+        if my_form.is_valid():
+            # now the data is good
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
+    context = {
+        "form" : my_form
+    }
+    return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     # print(request.GET)
+#     # print(request.POST)
+#     if request.method == "POST":
+#         my_new_title = request.POST.get('title')
+#         print(my_new_title)
+#         # Product.objects.create(title=my_new_title)
+#     context = {}
+#     return render(request, "products/product_create.html", context)
 
 # def product_create_view(request):
 #    form = ProductForm(request.POST or None)
@@ -19,7 +40,6 @@ def product_create_view(request):
 #         'form': form
 #        }
 #        return render(request, "products/product_create.html", context)
-    
 
 
 def product_detail_view(request):
@@ -29,7 +49,6 @@ def product_detail_view(request):
         'description' : obj.description
     } '''
     context = {
-        'object' : obj
+        'object': obj
     }
     return render(request, "products/product_detail.html", context)
-
