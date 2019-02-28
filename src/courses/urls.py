58242167de40c0b6059bc637d615a8492397bd74
antocaroca <antonella.caroca@cci-entel>
2019-02-28 13:09:38 -0300

    raw create class based view

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from .views import(
     CourseView,
+    CourseListView,
+    CourseCreateView,
     # my_fbv,
 )
 
@@ -8,5 +10,8 @@ app_name = 'courses'
 
 urlpatterns = [
     path('', CourseView.as_view(template_name='contact.html'), name='courses-list'),
+    path('create/', CourseCreateView.as_view(), name='course-create'),
+    path('', CourseListView.as_view(), name='courses-list'),
     # path('', my_fbv, name='courses-list'),
+    path('<int:id>/', CourseView.as_view(), name='courses-detail'),
 ]
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,17 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from .forms import CourseModelForm
+from .models import Course
 
 # BASED VIEW CLASS = VIEW
 
-class CourseView(View):
-    template_name = "about.html"
+class CourseCreateView(View):
+    template_name = "courses/course_create.html" 
     def get(self, request, *args, **kwargs):
         # GET method
-        return render(request, self.template_name, {})
+        form = CourseModelForm()
+        context = {"form" : form}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        # POST method
+        form = CourseModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = CourseModelForm()
+        context = {"form" : form}
+        return render(request, self.template_name, context)
+
+class CourseListView(View):
+    template_name = 'Courses/course_list.html'
+    queryset = Course.objects.all()
+
+    def get_quesryset(self):
+        return self.queryset
+
+    def get(self, request, *args, **kwargs):
+        context = {'object_list': self.get_queryset()}
+        return render(request, self.template_name, context)
+
+class CourseView(View):
+    template_name = "courses/course_detail.html" # DetailView
+    def get(self, request, id=None, *args, **kwargs):
+        # id=None means the id is no longer required
+        # GET method
+        context = {}
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+            context['object'] = obj
+        return render(request, self.template_name, context)
 
     # def post(request, *args, **kwargs):
     #     return render(request, 'about.html', {})
 
 # Http methods
+# This is a standard funcion based view. All it does is render a template
 def my_fbv(request, *args, **kwargs):
     return render(request, 'about.html', {})
